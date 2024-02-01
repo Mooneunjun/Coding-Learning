5cdbf80d85dd11d5e1f49753bfbeddc1c770668f
Mooneunjun <mooneunjun@icloud.com>
2024-02-02 00:35:41 +0900

    no message

--- test.js
+++ test.js
@@ -1,3 +1,28 @@
-//  hello world 출력
+// 싱글 리터럴  객체
+const object = {
+  property: "value",
+  method: function () {},
+};
 
-console.log("hello world");
+function NewObject(name) {
+  this.name = name;
+}
+
+const newObject = new NewObject("John");
+
+const newObject2 = Object.create(Object.prototype, {
+  name: {
+    value: "Jang",
+    writable: true, //덮어쓸 수 있는지 여부
+    enumerable: true, // 열거 가능한지 여부
+    configurable: true, //  삭제 가능한지 여부
+  },
+});
+
+newObject2.name = "kim";
+
+for (const key in newObject2) {
+  console.log(key);
+}
+
+newObject2;
