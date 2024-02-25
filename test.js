06058655a745c714ae5c73c478f4073502f5aa92
Mooneunjun <mooneunjun@icloud.com>
2024-02-25 13:18:34 +0900

    no message

--- test.js
+++ test.js
@@ -39,5 +39,27 @@ const person = {
   },
 };
 
+//
+
 person.firstName = 1234;
 console.log(person.firstName); // 알 수 없는 이름
+
+// Path: test.js
+
+const person = {
+  _firstName: "Moon",
+  _lastMame: "Eun-jun",
+
+  get firstName() {
+    return this._firstName.toUpperCase();
+  },
+
+  set firstName(newFirstName) {
+    if (typeof newFirstName != "string") {
+      this._firstName = "알 수 없는 이름";
+
+      return;
+    }
+    this._firstName = newFirstName;
+  },
+};
