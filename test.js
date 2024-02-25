80050103cc1e03631afe0be1fb259fc804292c0c
Mooneunjun <mooneunjun@icloud.com>
2024-02-25 15:13:09 +0900

    이름 변경

--- test.js
+++ test.js
@@ -1,65 +1,10 @@
 const person = {
-  _firstName: "Moon",
-  _lastMame: "Eun-jun",
+  firstNam: "jang",
+  LastName: "hyeon-seok",
 
   get firstName() {
-    return this._firstName.toUpperCase();
-  },
-
-  set firstName(newFirstName) {
-    if (typeof newFirstName != "string") {
-      this._firstName = "알 수 없는 이름";
-
-      return;
-    }
-    this._firstName = newFirstName;
+    return this.firstNam;
   },
 };
 
-// person.firstName = 1234;
-console.log(person.firstName); // MOON
-
-// Path: test.js
-
-const person = {
-  _firstName: "Moon",
-  _lastMame: "Eun-jun",
-
-  get firstName() {
-    return this._firstName.toUpperCase();
-  },
-
-  set firstName(newFirstName) {
-    if (typeof newFirstName != "string") {
-      this._firstName = "알 수 없는 이름";
-
-      return;
-    }
-    this._firstName = newFirstName;
-  },
-};
-
-//
-
-person.firstName = 1234;
-console.log(person.firstName); // 알 수 없는 이름
-
-// Path: test.js
-
-const person = {
-  _firstName: "Moon",
-  _lastMame: "Eun-jun",
-
-  get firstName() {
-    return this._firstName.toUpperCase();
-  },
-
-  set firstName(newFirstName) {
-    if (typeof newFirstName != "string") {
-      this._firstName = "알 수 없는 이름";
-
-      return;
-    }
-    this._firstName = newFirstName;
-  },
-};
+console.log(person.firstName); // jang
